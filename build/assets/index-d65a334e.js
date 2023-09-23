21ad65be7dcd50ea5a35665c207d9e744a969625
chanwoo hwang <302chanwoo@gmail.com>
2023-09-24 00:17:59 +0900

    build

--- build/assets/index-d65a334e.js
+++ build/assets/index-d65a334e.js
@@ -6,7 +6,7 @@
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${c}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript 1</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
